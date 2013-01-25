c50a2255be1d219b323319725d8c180281338e29
tim78317 <tliebl@my.wctc.edu>
2013-01-24 20:43:58 -0600

    class 2 homework first commit

---
+++ src/class2/java.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class2;
+
+/**
+ *
+ * @author tim78317
+ */
+public class java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/concrete/SalariedEmployee.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concrete;
+
+/**
+ *
+ * @author tim78317
+ */
+public class SalariedEmployee {
+    
+}
